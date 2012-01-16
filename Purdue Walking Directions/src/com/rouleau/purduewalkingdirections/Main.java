143ee043d0e2a9e1ffd5e01c9b9cdb5cb553877e
Caleb Rouleau <calebrouleau@gmail.com>
2012-01-15 21:50:07 -0500

    test 2

--- Purdue Walking Directions/src/com/rouleau/purduewalkingdirections/Main.java
+++ Purdue Walking Directions/src/com/rouleau/purduewalkingdirections/Main.java
@@ -1,5 +1,5 @@
 package com.rouleau.purduewalkingdirections;
-//calebrouleau was here
+//calebrouleau was here * 2
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
